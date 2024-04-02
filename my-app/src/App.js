c52f000002c609c19782f9590ca60acae0926799
Fernando Fong <fernandofong@ciencias.unam.mx>
2024-04-02 21:23:01 +0000

    feat: agregando cosas de front y datos simulados para la práctica

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,45 @@
-import './App.css';
+import React, { useState } from "react";
 
-import Alumnos from './components/Alumnos/Alumnos';
-import NuevoAlumno from './components/NuevoAlumno/NuevoAlumno';
+import "./App.css";
+
+import Alumnos from "./components/Alumnos/Alumnos";
+import NuevoAlumno from "./components/NuevoAlumno/NuevoAlumno";
 
 function App() {
-  
-  const alumnos = [
-    {
-      "nombre": "Fernando",
-      "apellido": "Fong",
-      "numCta": 313320679
-    },
-    {
-      "nombre": "Valeria",
-      "apellido": "Garcia",
-      "numCta": 314006088
-    },
-    {
-      "nombre": "Erick",
-      "apellido": "Martinez",
-      "numCta": 414890123
-    }
-  ];
+  const [peliculas, setPeliculas] = useState([
+    { id: 1, title: "Spiderman", director: "Sam R." },
+    { id: 2, title: "Batman", director: "Tim Burton" },
+    { id: 3, title: "Inception", director: "Christopher Nolan", inventario: 4 },
+  ]);
 
-  const agregarAlumno = alumno => {
-    console.log("In App.js")
-  }
+  const [alumnos, setAlumnos] = useState([
+    {
+      nombre: "Fernando",
+      apellido: "Fong",
+      numCta: 313320679,
+    },
+    {
+      nombre: "Valeria",
+      apellido: "Garcia",
+      numCta: 314006088,
+    },
+    {
+      nombre: "Erick",
+      apellido: "Martinez",
+      numCta: 414890123,
+    },
+  ]);
+
+  const agregarAlumno = (alumno) => {
+    const nuevoAlumno = [alumno, ...alumnos];
+    setAlumnos(nuevoAlumno);
+    console.log(nuevoAlumno);
+  };
 
   return (
     <div className="App">
       <NuevoAlumno onAgregarAlumno={agregarAlumno} />
-      <Alumnos alumnos={alumnos} /> 
+      <Alumnos alumnos={alumnos} />
     </div>
   );
 }
--- my-app/src/components/NuevoAlumno/AlumnoForm/AlumnoForm.js
+++ my-app/src/components/NuevoAlumno/AlumnoForm/AlumnoForm.js
@@ -1,73 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import './AlumnoForm.css';
+import "./AlumnoForm.css";
 
 const AlumnoForm = (props) => {
+  const [nombreIngresado, setNombreIngresado] = useState("");
+  const [apellidoIngresado, setApellidoIngresado] = useState("");
+  const [numCtaIngresado, setNumCtaIngresado] = useState("");
 
-    const [nombreIngresado, setNombreIngresado] = useState('');
-    const [apellidoIngresado, setApellidoIngresado] = useState('');
-    const [numCtaIngresado, setNumCtaIngresado] = useState('');
+  const cambioNombreHandler = (event) => {
+    setNombreIngresado(event.target.value);
+  };
 
-    const cambioNombreHandler = (event) => {
-        setNombreIngresado(event.target.value);
-    }
+  const cambioApellidoHandler = (event) => {
+    setApellidoIngresado(event.target.value);
+  };
 
-    const cambioApellidoHandler = (event) => {
-        setApellidoIngresado(event.target.value);
-    }
+  const cambioNumCtaHandler = (event) => {
+    setNumCtaIngresado(event.target.value);
+  };
 
-    const cambioNumCtaHandler = (event) => {
-        setNumCtaIngresado(event.target.value);
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-
-        const alumno = {
-            "nombre": nombreIngresado,
-            "apellido": apellidoIngresado,
-            "numCta": numCtaIngresado
-        }
-
-        props.onGuardarAlumno(alumno);
-        setNombreIngresado('');
-        setApellidoIngresado('');
-        setNumCtaIngresado('');
+    const alumno = {
+      nombre: nombreIngresado,
+      apellido: apellidoIngresado,
+      numCta: numCtaIngresado,
     };
+    if (
+      nombreIngresado === "" ||
+      apellidoIngresado === "" ||
+      numCtaIngresado === ""
+    ) {
+      alert("Campos vacíos!!");
+      return;
+    }
+    props.onGuardarAlumno(alumno);
+    setNombreIngresado("");
+    setApellidoIngresado("");
+    setNumCtaIngresado("");
+  };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <div className="nuevo-alumno__controls">
-                <div className="nuevo-alumno__control">
-                    <label>Nombre: </label>
-                    <input
-                        type="text"
-                        value={nombreIngresado}
-                        onChange={cambioNombreHandler}
-                    />
-                </div>
-                <div className="nuevo-alumno__control">
-                    <label>Apellido: </label>
-                    <input
-                        type="text"
-                        value={apellidoIngresado}
-                        onChange={cambioApellidoHandler}
-                    />
-                </div>
-                <div className="nuevo-alumno__control">
-                    <label>Num. Cta.: </label>
-                    <input
-                        type="text"
-                        value={numCtaIngresado}
-                        onChange={cambioNumCtaHandler}
-                    />
-                </div>
-                <div className="nuevo-alumno__actions">
-                    <button type="submit">Agregar alumno</button>
-                </div>
-            </div>
-        </form>
-    );
-}
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="nuevo-alumno__controls">
+        <div className="nuevo-alumno__control">
+          <label>Nombre: </label>
+          <input
+            type="text"
+            value={nombreIngresado}
+            onChange={cambioNombreHandler}
+          />
+        </div>
+        <div className="nuevo-alumno__control">
+          <label>Apellido: </label>
+          <input
+            type="text"
+            value={apellidoIngresado}
+            onChange={cambioApellidoHandler}
+          />
+        </div>
+        <div className="nuevo-alumno__control">
+          <label>Num. Cta.: </label>
+          <input
+            type="text"
+            value={numCtaIngresado}
+            onChange={cambioNumCtaHandler}
+          />
+        </div>
+        <div className="nuevo-alumno__actions">
+          <button type="submit">Agregar alumno</button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default AlumnoForm;
